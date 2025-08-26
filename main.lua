4aa65cf6a1ca7650541853a563bf7b4adeb98383
loiismebitch <loioithuyne@gmail.com>
2025-08-26 11:50:07 +0700

    main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+-- main.lua
+local Tracker = loadstring(game:HttpGet("https://raw.githubusercontent.com/loiismebitch/tracker-stats-priv/main/tracker.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loiismebitch/tracker-stats-priv/main/gui.lua"))()
